d88af3131fd7df712c81c625e9a968267e5860e4
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-09-28 11:30:53 +0530

    Added QuadraticRoots

---
+++ QuadraticRoots.java
@@ -0,0 +1,21 @@
+public class QuadraticRoots {
+    public static void main(String[] args) {
+        double a = 2.0;
+        double b = -5.0;
+        double c = 2.0;
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Root 1: " + root1);
+            System.out.println("Root 2: " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("Root: " + root);
+        } else {
+            System.out.println("The quadratic equation has no real roots.");
+        }
+    }
+}
