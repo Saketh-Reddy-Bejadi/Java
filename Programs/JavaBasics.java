830f91db23ca558bd99bacdd4300cb84340181be
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-10-01 11:03:37 +0530

    Modified

---
+++ Programs/JavaBasics.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+import java.util.*;
+public class JavaBasics {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+
+        float a=20.567f;
+        System.out.println((int)(a));
+
+
+
+        sc.close();
+
+    }
+}
