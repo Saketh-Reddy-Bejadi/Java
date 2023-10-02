0d7a8a367ba610787c93720f18fe63255d2ef0c7
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-10-02 22:37:36 +0530

    modified

--- Programs/JavaBasics.java
+++ Programs/JavaBasics.java
@@ -4,10 +4,17 @@ public class JavaBasics {
     public static void main(String args[]) {
         Scanner sc= new Scanner(System.in);
 
-        float a=20.567f;
-        System.out.println((int)(a));
+        // type conversion
+        // float a=20.567f;
+        // System.out.println((int)(a));
 
-
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Difference:"+(a-b));
+        System.out.println("Product:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        System.out.println("Modulo:"+a%b);
 
         sc.close();
 
