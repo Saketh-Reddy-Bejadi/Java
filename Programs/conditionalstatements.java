5d051d114d8836d24a5465aae17fea92702ff9cb
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-11-13 13:55:26 +0530

    modified

---
+++ Programs/conditionalstatements.java
@@ -0,0 +1,12 @@
+public class conditionalstatements {
+    public static void main(String[] args) {
+        //if
+        int salary=25000;
+        if(salary>=10000) {
+            salary+=2000;
+        }else{
+            salary+=1000;
+        }
+        System.out.println(salary);
+    }
+}
