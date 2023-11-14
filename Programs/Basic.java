79a1ae0e2b7b5272b9d2be653a471440bcca4f0c
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-11-14 22:12:16 +0530

    modified

---
+++ Programs/Basic.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Basic {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter Int:");
+        int a=sc.nextInt();
+        System.out.println("Int:"+a);
+
+        System.out.print("Enter Float:");
+        float b=sc.nextFloat();
+        System.out.println("Float:"+b);
+
+        System.out.print("Enter String(single):");
+        String c=sc.next();
+        System.out.println("String:"+c);
+
+        System.out.print("Enter String(multiple):");
+        String d=sc.nextLine();
+        System.out.println("String Line:"+d);
+
+
+
+
+
+        sc.close();
+    }
+}
