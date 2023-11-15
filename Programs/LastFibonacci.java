c1881d9ea660e4757faa544ab9d1e6db45f75b57
Saketh Reddy Bejadi <144515308+Saketh-Reddy-Bejadi@users.noreply.github.com>
2023-11-15 19:50:10 +0000

    modified

--- Programs/LastFibonacci.java
+++ Programs/LastFibonacci.java
@@ -1,16 +1,17 @@
+import java.util.Scanner;
 public class LastFibonacci{
     public static void main(String args[]){
-        int n,f0=0,f1=1,f2;
+        int n,f0=0,f1=1,f2=0;
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter no.of values to get:");
         n= sc.nextInt();
-        System.out.print(f0+" "+f1);
+       // System.out.print(f0+"\n"+f1);
         for(int i=2;i<=n;i++) {
             f2=f0+f1;
             f0=f1;
             f1=f2;
         }
         System.out.print(f2);
-
+sc.close();
     }
 }
