0f149cd5d916d1f0de36a6c4b9404279402a27cc
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-11-22 19:48:04 +0530

    modified

--- Programs/ArmstrongNumber.java
+++ Programs/ArmstrongNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ArmstrongNumber {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int num,sum;
+        // int num,sum;
         System.out.println("Enter a number:");
         String number=sc.next();
         for (int i=0;i<=number.length();i++) {
