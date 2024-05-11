ed7846551a0c8a50552c9f0a30defe7301df4be5
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-05-11 09:14:42 +0530

    modified

--- Lab Experiments/CSM/CSM_B.java
+++
@@ -1,8 +0,0 @@
-package CSM;
-public class CSM_B {
-    public static void Add(int a, int b) {
-        int c= a + b;
-        System.out.println("Addition of "+a+", "+b+" : "+c);
-    }
-}
-
--- Lab Experiments/CSM/CSM_C.java
+++
@@ -1,9 +0,0 @@
-package CSM;
-
-public class CSM_C {
-    public static void Sub(int a,int b) {
-        int c=a-b;
-        System.out.println("Difference of "+a+", "+b+" : "+c);
-    }
-}
-
