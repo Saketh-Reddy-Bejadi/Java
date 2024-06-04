469f30399550301b3ca50ac51b0c1b77cf5e86e9
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-06-04 21:37:16 +0530

    modified

---
+++ Learning/KthLargestElement.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        int[] nums={3,2,3,1,2,4,5,5,6};
+        int k =4;
+        System.out.println(kLargest(nums,k));
+    }
+    static int kLargest(int[] nums, int k){
+        Arrays.sort(nums);
+        int i =nums.length-k;
+        return nums[i];
+    }
+}
